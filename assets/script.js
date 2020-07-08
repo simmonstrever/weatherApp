10f769d6ef9d06eeb24a008472f9c2f77bc2770d
macT <simmonst208@gmail.com>
2020-07-08 01:42:54 -0600

    search bar complete, still working on 5day forecast and the api to draw the info

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var query = "&APPID=922170f09e7e847c60969243d491caad";
 
 //use moment for displaying UV light count
 var date = new Date();
+console.log(date);
 //create array that stores user inputs
 
 // ('https://api.openweathermap.org/data/2.5/weather?q=Denver&APPID=922170f09e7e847c60969243d491caad')
@@ -11,7 +12,9 @@ $("#search").on("click", function(){
     var location = $("#search-text").val();
     // var location = $(this).attr("cityName");
     //if (location === "") use geo location
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&units=imperial&APPID=922170f09e7e847c60969243d491caad";
+    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&units=imperial&appid=922170f09e7e847c60969243d491caad";
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q="+location+"&appid=922170f09e7e847c60969243d491caad"
+    console.log(queryURL);
 
    
 
@@ -27,20 +30,31 @@ $("#search").on("click", function(){
         $("#temperature").text(response.main.temp);
         $("#humidity").text(response.main.humidity);
         $("#wind-speed").text(response.wind.speed);
-        //for later
-        // $("#uv").text()
-
-        var uvURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?"+query+"&lat=" + response.coord.lat+"&lon="+response.coord.lon+"&cnt=1";
+        $("#icon").text(response.weather.icon);
+        //need to render the icon
+        //console.log(response.weather.icon);
+    
+        var uvURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?"+query+"&lat=" + response.coord.lat+"&lon="+response.coord.lon+"&cnt=5";
         console.log(uvURL);
 
         $.ajax({
             url: uvURL,
             method: "GET"
         }).then(function(ultraviolet){
-            // Math.floor(ultraviolet.value);
-            //if uv index < 2
-            // attr green
-        })
+            console.log(JSON.stringify(ultraviolet));
+            var index = ultraviolet[1].value;
+            $("#uv").text(index);
+            console.log(index);
+            if (index < 10){
+                $("#uv").attr("style", "background-color: green");
+            } else if (index > 10 ){
+                $("#uv").attr("style", "background-color: red");
+            }
+            // console.log(ultraviolet);
+            // console.log(ultraviolet[1].value);
+        });
+
+
     })
     
 });
